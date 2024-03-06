49f1135a9b7ff5a9b74f8cef9942c320cacc91e8
BrunoDev@777 <brugui2703@gmail.com>
2024-03-05 23:06:07 -0300

    Parte 3 finalizada

--- introducao/src/App.js
+++ introducao/src/App.js
@@ -1,15 +1,17 @@
 import React,{useState} from "react";
 
-
 import MeusDados01 from "./components/atividade00/MeusDados01";
 
 import MeusDados02 from "./components/atividade00/MeusDados02";
+
+import { CelsiusParaFahrenheit, FahrenheitParaCelsius, Kelvin } from './components/atividade00/Temperatura03';
 
 function App() {
 
   const [nome,setNome] = useState("");
   const [curso, setCurso] = useState("");
   const [universidade, setUniversidade] = useState("");
+
 
   return (
 
@@ -21,7 +23,12 @@ function App() {
       <input type="text" value={universidade} onChange={(e) => setUniversidade(e.target.value)} placeholder="Universidade" />
       <MeusDados02 nome={nome} curso={curso} universidade={universidade} />
 
-      
+
+      <div>
+        <h3>{CelsiusParaFahrenheit(20)}</h3>
+        <h3>{FahrenheitParaCelsius(68)}</h3>
+        <h3>{Kelvin(293.15)}</h3>
+      </div>
 
     </div>
 
--- introducao/src/components/atividade00/Temperatura03.jsx
+++ introducao/src/components/atividade00/Temperatura03.jsx
@@ -1,24 +1,37 @@
+import React from 'react';
 
-const celsiusParaFahrenheit = (c) => {
-
+const CelsiusParaFahrenheit = (c) => {
     const Cel = c;
     const Fahr = Cel * 9 / 5 + 32;
-    return(    
+
+    return (
+        <div>
             <h3>Temperatura em Celsius: {Cel} ... Temperatura em Fahrenheit: {Fahr}</h3>
-        )
+        </div>
+    );
 }
 
-const fahrenheitParaCelsius = (f) => {
-
+const FahrenheitParaCelsius = (f) => {
     const Fahr = f;
     const Cel = (Fahr - 32) * 5 / 9;
-    return(    
+
+    return (
+        <div>
             <h3>Temperatura em Fahrenheit: {Fahr} ... Temperatura em Celsius: {Cel}</h3>
-        )
-
+        </div>
+    );
 }
 
-const kelvin = () => {
+const Kelvin = (k) => {
+    const Kev = k;
+    const Cel = (Kev - 273);
+    const Fahr = ((k-273.15)*1.8)+32;
 
-
+    return (
+        <div>
+            <h3>Temperatura em Kelvin: {Kev} ... Temperatura em Celsius: {Cel} ... Temperatura: {Fahr}</h3>
+        </div>
+    );
 }
+
+export { CelsiusParaFahrenheit, FahrenheitParaCelsius, Kelvin };
